8c0b468554dcb75583529477c0c703a2db7cfc9b
Leandro D Moreira <leandrodmoreira@gmail.com>
2020-06-10 16:57:53 -0300

    Meu primeiro commit

---
+++ SpeedtestV2.py
@@ -0,0 +1,36 @@
+#Versão 1.3 - 08/06/2020 - 16:20
+
+import speedtest
+from datetime import datetime
+import time
+import csv
+
+speed = speedtest.Speedtest()
+speed.get_best_server()
+
+
+while True:
+
+    now = datetime.now()
+
+    file = open('Log_SpeedTest.csv','a')
+
+    reader = "Timestamp, Download(Mbs), Upload(Mbps) \n"
+
+    download = repr(round(speed.download() / 1000 / 1000, 1))
+    upload = repr(round(speed.upload () / 1000 / 1000, 1))
+
+    textoBruto = now.strftime('%Y-%m-%d %H:%M') + ';' + download + ';' + upload + "\n"
+
+    texto = textoBruto.replace(".",",")
+
+    #file.write(reader)
+    file.write(texto)
+
+    file.close()
+
+    print(texto)
+
+    time.sleep(3600)
+
+   
